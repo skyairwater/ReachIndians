fc217c06a5e9d51953542fc9a2ac8b2a4a5e4df3
skyairwater@gmail.com <skyairwater@gmail.com>
2018-09-14 16:32:52 -0400

    initial commit

---
+++ e2e/src/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('ri-root h1')).getText();
+  }
+}
