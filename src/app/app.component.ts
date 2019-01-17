6629721c75352cf46f7840fd5390c69b1ede1b1c
skyairwater@gmail.com <skyairwater@gmail.com>
2019-01-16 22:49:49 -0500

    In the prod environment, redirecting the user from http to https

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,35 @@
-import { Component } from '@angular/core';
-import {SlimLoadingBarService} from 'ng2-slim-loading-bar';
-import { NavigationCancel,
-        Event,
-        NavigationEnd,
-        NavigationError,
-        NavigationStart,
-        Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { SlimLoadingBarService } from 'ng2-slim-loading-bar';
+import {
+  NavigationCancel,
+  Event,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router
+} from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'ri-root',
-  templateUrl: './app.component.html'  
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit() {
+    if (environment.production) {
+      if (location.protocol === 'http:') {
+        window.location.href = location.href.replace('http', 'https');
+      }
+    }
+  }
   constructor(private _loadingBar: SlimLoadingBarService, private _router: Router) {
     this._router.events.subscribe((event: Event) => {
       this.navigationInterceptor(event);
     });
+
+
   }
   private navigationInterceptor(event: Event): void {
     if (event instanceof NavigationStart) {
